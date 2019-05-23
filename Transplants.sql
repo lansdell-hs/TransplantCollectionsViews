f77609f443be347e20f1e794f31d69155757e3fb
Heather Lansdell <22349308+lansdell-hs@users.noreply.github.com>
2019-05-23 09:14:10 -0400

    Update Transplants.sql

--- Transplants.sql
+++ Transplants.sql
@@ -16,6 +16,12 @@ select
    tx.region,
    tx.transplant_center as center,
    tx.organ,
+   tx.donor_ty,
+   tx.final_stat,
+   tx.dcd,
+   
+   left(ns.zipcode,5) as tx_zip,
+   left(l.zipcode,5) as opo_zip,
 --Liver specific
    li.meld_peld_lab,
 --Kidney/Pancreas specific
@@ -25,9 +31,16 @@ select
    kp.creatinine,
    y.glomerularfr as adlt_glomerularfr,
    z.glomerularfr as ped_glomerularfr,
+   
 --Demographic descriptors from lookup tables   
    e.descrip as ethnicity,
    cd.descrip as status,
+--Include foreign donors in deceased   
+   case when tx.donor_ty in ('Dec','For') 
+		then 'Dec'
+		else 'Liv'
+	end as donorty,
+   
 --Roll up specific diagnostic codes into committee approved higher level categories.   
    
    CASE WHEN tx.diag IN (3XXX,30XX,3XXX,3XXX,3XXX,XXX4,XXX5,XXXXX6,XXXXX6,XXXX8,XXXXXX4,XXXXXX1,XXX2,
@@ -116,25 +129,25 @@ select
 	end as donorty,
    
 --Set categorical bins on LAS scores   
-   CASE WHEN TH.match_las_stop between 0 and 20 
+   CASE WHEN TH.match_las_stop between 0 and 19 
       then '<20'  
        
-      WHEN TH.match_las_stop between 20 and 30          
+      WHEN TH.match_las_stop between 20 and 29          
       then '20-<30' 
          
-      WHEN TH.match_las_stop between 30 and 35
+      WHEN TH.match_las_stop between 30 and 34
       then '30-<35' 
          
-      WHEN TH.match_las_stop between 35 and 40       
+      WHEN TH.match_las_stop between 35 and 39       
       then '35-<40'
           
-      WHEN TH.match_las_stop between 40 and 50   
+      WHEN TH.match_las_stop between 40 and 49   
       then '40-<50'
           
-      WHEN TH.match_las_stop between 50 and 60         
+      WHEN TH.match_las_stop between 50 and 59         
       then '50-<60'
           
-      WHEN TH.match_las_stop between 60 and 70 
+      WHEN TH.match_las_stop between 60 and 69 
       then '60-<70' 
          
       WHEN TH.match_las_stop between 70 and 1000    
@@ -142,7 +155,7 @@ select
           
    End as LAS_tx,
 --Set categorical bins on Ages    
-   CASE WHEN tx.AGE between 0 and 1 
+   CASE WHEN tx.AGE <1 
       then '<1'
 	   
       WHEN tx.AGE between 1 and 5 
@@ -170,8 +183,37 @@ select
       then '65+'
 	
 	End as Age_at_TX,
+--Donor Ages
+	CASE WHEN tx.AGE_donor <1
+		then '<1'
+		
+		WHEN tx.AGE_donor between 1 and 5 
+		then '1-5'
+		
+		WHEN tx.AGE_donor between 6 and 10 
+		then '6-10'
+		
+		WHEN tx.AGE_donor between 11 and 17 
+		then '11-17'
+		
+		WHEN tx.AGE_donor between 18 and 26 
+		then '18-26'
+		
+		WHEN tx.AGE_donor between 26 and 34 
+		then '26-34'
+		
+		WHEN tx.AGE_donor between 35 and 49 
+		then '35-49'
+		
+		WHEN tx.AGE_donor between 50 and 64 
+		then '50-64'
+		
+		WHEN tx.AGE_donor between 65 and 1000 
+		then '65+'
+	
+        End as Donor_Age,
 --Roll up Blood subgroups   
-   CASE WHEN tx.ABO in ('A','A1','A2','A2B') 
+   CASE WHEN tx.ABO in ('A','A1',,'A1B','A2','A2B') 
       then 'A'
 	   else tx.ABO
 	end as blood_type,
@@ -192,7 +234,7 @@ select
 
 	End as CPRA_tx, 
 --Set categorical bins on MELD/PELD at Transplant   
-   CASE WHEN tx.organ='LI' and li.meld_peld_lab between 0 and 15 
+   CASE WHEN tx.organ='LI' and li.meld_peld_lab between 0 and 14 
       then 'MELD/PELD <15'
 	   
       WHEN tx.organ='LI' and li.meld_peld_lab between 15 and 22 
@@ -208,6 +250,31 @@ select
       then 'Not Reported'
 
 	End as LabMeldPeld,
+	
+--Handling Liver Status
+CASE WHEN tx.organ='LI' and tx.Final_stat=6XXX then 'Status 1'
+	WHEN tx.organ='LI' and tx.Final_stat=6XXX then 'Status 1B'
+    WHEN tx.organ='LI' and tx.Final_stat=6XXX	then 'Old status 4'
+	WHEN tx.organ='LI' and tx.Final_stat=6XXX	then 'Status 3'
+	WHEN tx.organ='LI' and tx.Final_stat=XXXX	then 'Status 2B'
+	WHEN tx.organ='LI' and tx.Final_stat=XX9	then 'Temporarily Inactive'
+	WHEN tx.organ='LI' and tx.Final_stat=XX2	then 'Old status 2'
+	WHEN tx.organ='LI' and tx.Final_stat=XX1	then 'Status 1A'
+	WHEN tx.organ='LI' and tx.Final_stat=XX0	then 'Status 2A'
+	WHEN tx.organ='LI' and tx.Final_stat between 6XXX and 6XXX then 'MELD/PELD <15'
+    WHEN tx.organ='LI' and tx.Final_stat  between XXXX and XXXX then 'MELD/PELD 15-22'
+    WHEN tx.organ='LI' and tx.Final_stat between  XXXX and XXXX then 'MELD/PELD 23-34'
+    WHEN tx.organ='LI' and tx.Final_stat between  XXXX and XXXX then 'MELD/PELD 35+'
+    WHEN tx.organ='LI' and tx.Final_stat is NULL or tx.end_stat=XXXX then 'Not Reported'
+	else cd.descrip
+	End as TXStat,
+--Set categorical bins on EPTS 
+    CASE WHEN tx.organ in ('KI','KP') and kp.stop_epts between X and XXX then '0-20%'
+    WHEN tx.organ in ('KI','KP') and kp.stop_epts  >XXX then '>20%'
+    WHEN tx.organ in ('KI','KP') and kp.stop_epts  is NULL then 'Not Reported'
+    
+    End as EPTS_tx,
+
 --Set categorical bins on Donor KDPI
    CASE WHEN tx.organ in ('KI','KP') and kp.KDPI between 0 and .35 
       then '0-35'
@@ -265,8 +332,36 @@ select
    End
    as glomerularfr_final 
 
+case when datepart(quarter,tx.transplant_dt)=1 then concat('March 31,',DATEPART(year,tx.transplant_dt)) 
+	when datepart(quarter,tx.transplant_dt)=2 then concat('June 30,',DATEPART(year,tx.transplant_dt)) 
+	when datepart(quarter,tx.transplant_dt)=3 then concat('September 30,',DATEPART(year,tx.transplant_dt)) 
+	when datepart(quarter,tx.transplant_dt)=4 then concat('December 31,',DATEPART(year,tx.transplant_dt)) 
+	end as caldate,	
+										      
+case when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) <8 then '0-7 days'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 8 and 31 then '8-31 days'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 32 and 90 then '1-3 months'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 91 and 186 then '3-6 months'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 187 and 365 then '6-12 months'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 366 and 730 then '1 to 2 years'
+when coalesce(kp.lenofstay,th.lenofstay,li.lenofstay) between 731 and 6000 then '2+ years'
+end as los,
+
+coalesce(th.grfdate,kp.grfdate_ki,kp.grfdate_pa,li.grfdate) as grffaildate,
+
+coalesce(th.grfstatus,kp.GrfSTATUS_KI,kp.GrfSTATUS_PA,li.grfstatus) as gstatus,
+										    
+										      
 from
    SomeResearchDB.DSV.TX tx 
+   left join
+      OrgAdminDB..Institutes ns on tx.ctr=ns.ctr4+'-'+ns.ctrty	
+   left join
+      OrgAdminDB...ServiceOpo d on tx.ctr=d.txctr4+'-'+txctrty									      
+   left join	
+      OrgAdminDB..Institutes l on tx.ctr=ns.ctr4+'-'+l.ctrty									      
+   left join									      
+      SomeResearchDB.DSV.donor on fd.donorid=tx.donorid										      
    left join
       SomeResearchDB.DSV.kidpan kp 
       on coalesce(kp.transplant_id_ki, kp.transplant_id_pa) = tx.transplant_id 
